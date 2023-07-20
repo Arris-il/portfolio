329b045bded10443d2bdd560c3a1a3db8d9c0de8
Arris174 <91550019+Arris174@users.noreply.github.com>
2023-07-20 23:31:34 +0300

    Push

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,6 +1,6 @@
-const btnDarkMode = document.querySelector(".dark-mode-btn");
-
 /* Dark Theme settings */
+
+const btnDarkMode = document.querySelector(".dark-mode-btn");
 
 //Check if system with dark theme
 
@@ -49,3 +49,92 @@ btnDarkMode.onclick = function () {
         localStorage.setItem("darkMode", "light");
     }
 };
+
+
+
+function isElementInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+function handleScroll() {
+    const links = document.querySelectorAll('.nav-list__link');
+
+    links.forEach(link => {
+        const sectionId = link.getAttribute('href');
+        const section = document.querySelector(sectionId);
+
+        if (isElementInViewport(section)) {
+            // Удаляем класс у всех кнопок перед добавлением его к активной кнопке
+            links.forEach(link => link.classList.remove('nav-list__link--active'));
+            link.classList.add('nav-list__link--active');
+        } else {
+            link.classList.remove('nav-list__link--active');
+        }
+    });
+}
+
+window.addEventListener('scroll', handleScroll)
+
+
+
+const btnUp = {
+    el: document.querySelector('.btn-up'),
+    scrolling: false,
+    show() {
+        if (this.el.classList.contains('btn-up_hide') && !this.el.classList.contains('btn-up_hiding')) {
+            this.el.classList.remove('btn-up_hide');
+            this.el.classList.add('btn-up_hiding');
+            window.setTimeout(() => {
+                this.el.classList.remove('btn-up_hiding');
+            }, 300);
+        }
+    },
+    hide() {
+        if (!this.el.classList.contains('btn-up_hide') && !this.el.classList.contains('btn-up_hiding')) {
+            this.el.classList.add('btn-up_hiding');
+            window.setTimeout(() => {
+                this.el.classList.add('btn-up_hide');
+                this.el.classList.remove('btn-up_hiding');
+            }, 300);
+        }
+    },
+    addEventListener() {
+        // при прокрутке окна (window)
+        window.addEventListener('scroll', () => {
+            const scrollY = window.scrollY || document.documentElement.scrollTop;
+            if (this.scrolling && scrollY > 0) {
+                return;
+            }
+            this.scrolling = false;
+            // если пользователь прокрутил страницу более чем на 200px
+            if (scrollY > 400) {
+                // сделаем кнопку .btn-up видимой
+                this.show();
+            } else {
+                // иначе скроем кнопку .btn-up
+                this.hide();
+            }
+        });
+        // при нажатии на кнопку .btn-up
+        document.querySelector('.btn-up').onclick = () => {
+            this.scrolling = true;
+            this.hide();
+            // переместиться в верхнюю часть страницы
+            window.scrollTo({
+                top: 0,
+                left: 0,
+                behavior: 'smooth'
+            });
+        }
+    }
+}
+
+btnUp.addEventListener();
+
+
